ca433908424425dd0d537600035ef874f405138b
lizhiyong <lizhiyong@123.com>
2020-10-27 21:32:33 +0800

    captcher code unfinished, is closer

---
+++ test20201027/matplotlib/test01.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import random
+
+x1 = random.randint(5, 20)
+y1 = random.randint(5, 20)
+x2 = random.randint(190, 230)
+y2 = random.randint(190, 230)
+res = [x1, y1, x2, y2]
+xyxy = tuple(res)
+print(xyxy)
+
+print(type(np.random.choice(["red", "blue", "yellow", "gray", "black", "white"])))
+
+
+
+
+
+
+
+
+
