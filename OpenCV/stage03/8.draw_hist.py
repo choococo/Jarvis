b3a9f75a5f72356bc8d526de40991393a6c33093
lizhiyong <lizhiyong@123.com>
2020-11-08 19:29:19 +0800

    test github

--- OpenCV/stage03/8.draw_hist.py
+++ OpenCV/stage03/8.draw_hist.py
@@ -7,4 +7,7 @@ img = cv2.imread("../images/30.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)            # 转换为灰度图
 
 # 绘制灰度直方图
-hist_gray = cv2.calcHist()
+hist_gray = cv2.calcHist(images=[gray], channels=[0], mask=None, histSize=[256], ranges=[0, 255])
+cv2.imshow("hist_gray", hist_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
