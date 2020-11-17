98591f3a07c204afa267409789f5274ab0c6caf7
lizhiyong <lizhiyong@123.com>
2020-11-17 19:33:18 +0800

    update worksapce

--- OpenCV/targetDection/wirtetxt.py
+++
@@ -1,9 +0,0 @@
-import os
-
-with open("./label/list_new_bbox.txt", "w") as f:
-    for i in range(10):
-        strs = ['1', '2', '3']
-        strs = " ".join(strs)
-        f.writelines(strs)
-        f.flush()
-        f.write("\n")
